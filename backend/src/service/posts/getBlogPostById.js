d0fd144289b33be68bb71156c1d558e0455c6b12
NikolaSretko <nikola.sretkovic99@gmail.com>
2024-03-18 15:28:10 +0100

    populate author

--- backend/src/service/posts/getBlogPostById.js
+++ backend/src/service/posts/getBlogPostById.js
@@ -1,7 +1,8 @@
 import Post from "../../models/Post.js";
 
 export async function getBlogPostById(postId) {
-    const blogPost = await Post.findById(postId);
+    const blogPost = await Post.findById(postId)
+        .populate("author", "username avatar profession qrcode");
     if (!blogPost) throw new Error("Post post not found");
     return blogPost;
 }
