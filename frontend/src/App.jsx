20359acd8f8a798abbbe6d28ab69884f8bf4b591
huangrunhong <huang.run.hong.1986@gmail.com>
2024-03-18 09:45:24 +0100

    proxy added AuthenrizationProvider added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,27 +17,27 @@ import ForgotPassword from "./components/forgotpassword/ForgotPassword.jsx";
 function App() {
   return (
     <BrowserRouter>
-      {/* <AuthorizationContextProvider> */}
-      <UserContextProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/sixdigit/:userId" element={<SixDigit />} />
-          <Route path="/search" element={<Search />} />
+      <AuthorizationContextProvider>
+        <UserContextProvider>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/sixdigit/:userId" element={<SixDigit />} />
+            <Route path="/search" element={<Search />} />
 
-          <Route path="/upload" element={<UserPostUpload />} />
-          <Route path="/profile" element={<UserProfile />} />
-          <Route path="/comment" element={<Comments />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route
-            path="/:userId/other-user-profile"
-            element={<OtherUserProfile />}
-          />
-          <Route path="/resend-password" element={<ForgotPassword />} />
-        </Routes>
-      </UserContextProvider>
-      {/* </AuthorizationContextProvider> */}
+            <Route path="/upload" element={<UserPostUpload />} />
+            <Route path="/profile" element={<UserProfile />} />
+            <Route path="/comment" element={<Comments />} />
+            <Route path="/edit-profile" element={<EditProfile />} />
+            <Route
+              path="/:userId/other-user-profile"
+              element={<OtherUserProfile />}
+            />
+            <Route path="/resend-password" element={<ForgotPassword />} />
+          </Routes>
+        </UserContextProvider>
+      </AuthorizationContextProvider>
     </BrowserRouter>
   );
 }
