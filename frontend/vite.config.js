4bcc369ba1d47c590a8978c7a4ec5fcb29974b15
NikolaSretko <nikola.sretkovic99@gmail.com>
2024-03-20 18:20:06 +0100

    backendurl

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { backendUrl } from "./src/api";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +7,7 @@ export default defineConfig({
 
   server: {
     proxy: {
-      "/api": backendUrl,
+      "/api": "http://localhost:4444",
     },
   },
 });
