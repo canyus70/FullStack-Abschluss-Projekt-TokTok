40a98abf8460a1b2ac21a2136eb09c6758485da9
NikolaSretko <nikola.sretkovic99@gmail.com>
2024-03-21 11:30:33 +0100

    deployed

--- backend/src/service/posts/getAllBlogPosts.js
+++ backend/src/service/posts/getAllBlogPosts.js
@@ -1,7 +1,7 @@
 import Post from "../../models/Post.js";
 
 export async function getAllBlogPosts() {
-    const posts = await Post.find({}).populate('comments').populate('author', "username firstname lastname ").exec(1);
+    const posts = await Post.find({}).populate('comments').populate('author', "username firstname lastname avatar").exec(1);
     if (posts.length === 0) throw new Error("No blog posts found");
     return posts;
 }
--- backend/src/service/posts/getBlogPostById.js
+++ backend/src/service/posts/getBlogPostById.js
@@ -4,7 +4,7 @@ export async function getBlogPostById(postId) {
 
   const blogPost = await Post.findById(postId)
     .populate("author", "username avatar profession qrcode")
-    .populate("comments", "author createdAt message");
+    .populate("comments", "author createdAt message ");
   if (!blogPost) throw new Error("Post post not found");
   return blogPost;
 }
